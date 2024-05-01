dc0fa7faebcee6c531cba81ca4d0fa88d1873204
LAPTOP-OGIMQDJV\Cunanan <denvercunanan26@gmail.com>
2024-05-01 22:58:14 +0800

    datetime

--- php/get_time.php
+++
@@ -1,5 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Manila'); // Set the timezone to Philippines
-$currentTime = date('h:i:s A'); // Format the time
-echo $currentTime; // Return the formatted time
-?>
